5bbb93c6f32d9b9be8f40eb8ae8ac360813176cc
sugar191 <pawaburo@gmail.com>
2025-06-29 11:01:49 +0900

    管理画面にCSVを追加

--- songs/admin.py
+++ songs/admin.py
@@ -1,5 +1,40 @@
 from django.contrib import admin
 from .models import Artist, Song, Rating
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+
+# 共通のベース管理クラス
+class BaseResourceAdmin(ImportExportModelAdmin):
+    pass
+
+
+class ArtistResource(resources.ModelResource):
+    class Meta:
+        model = Artist
+
+
+class ArtistAdmin(BaseResourceAdmin):
+    resource_class = ArtistResource
+
+
+class SongResource(resources.ModelResource):
+    class Meta:
+        model = Song
+
+
+class SongAdmin(BaseResourceAdmin):
+    resource_class = SongResource
+
+
+class RatingResource(resources.ModelResource):
+    class Meta:
+        model = Rating
+
+
+class RatingAdmin(BaseResourceAdmin):
+    resource_class = RatingResource
+
 
 admin.site.register(Artist)
 admin.site.register(Song)
