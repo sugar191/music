9c6c8643f21b49559a68cddce26da579a1f0e5b5
sugar191 <pawaburo@gmail.com>
2025-09-03 10:23:25 +0900

    Revert "api化"
    This reverts commit ca69b3c1fc4943c44665204b3c280b1719c4bb33.

--- music/urls.py
+++ music/urls.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/login/", auth_views.LoginView.as_view(), name="login"),  # ログイン
     path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
-    path("api/", include("songs.urls_api")),
-    path("api/auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("", include("songs.urls")),
 ]
