e5741a4050d6edbc664e6f3134bc6bb4af7566e7
sugar191 <pawaburo@gmail.com>
2025-10-22 13:23:04 +0900

    バグ修正

--- songs/api_urls.py
+++ songs/api_urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
         api_views.SongsRatingExport.as_view(),
         name="songs-rating-export",
     ),
-    path("get_song", api_views.get_song.as_view(), name="get_song"),
+    path("get_song", api_views.get_song, name="get_song"),
 ]
